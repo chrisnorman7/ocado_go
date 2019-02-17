d4d9bd926ef181c1f2a578f776cec24d1c97dbc4
Chris Norman <chris.norman2@googlemail.com>
2019-02-17 08:30:28 +0000

    Load results into an iframe.

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const string = document.getElementById("string")
-const search = document.getElementById("search")
+function loadSearchResults() {
+    searchButton.disabled = false
+    searchButton.value = "Search"
+    let html = frame.contentDocument || frame.contentWindow
+    if (html.document) html = html.document
+    output.innerText = "Works."
+}
 
-search.onsubmit = (e) => {
+const frame = document.getElementById("frame")
+const baseUrl = "https://www.ocado.com/search?entry="
+const string = document.getElementById("string")
+const searchForm = document.getElementById("searchForm")
+const searchButton = document.getElementById("searchButton")
+const output = document.getElementById("output")
+
+window.onload = () => {
+    frame.sandbox = "allow-same-origin allow-scripts"
+    frame.hidden = true
+    frame.onload = loadSearchResults
+}
+
+searchForm.onsubmit = (e) => {
     e.preventDefault()
-    let value = string.value
-    alert(value)
+    let value = escape(string.value)
+    if (!value) {
+        return alert("You must search for something.")
+    }
+    let url = baseUrl + value
+    searchButton.disabled = true
+    searchButton.value = "Loading..."
+    output.innerText = "Loading results, please wait."
+    frame.src = url
 }
