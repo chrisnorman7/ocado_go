e58f52602f158af45fc31e0207d9de4ea416b179
Chris Norman <chris.norman2@googlemail.com>
2019-02-17 12:03:18 +0000

    Mostly works now.

---
+++ main.py
@@ -0,0 +1,82 @@
+"""Ocado Go server."""
+
+from json import loads
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify, render_template, request
+from requests import Session
+
+http = Session()
+base_url = 'https://www.ocado.com/search?entry='
+app = Flask(__name__)
+
+
+def error(msg):
+    """Return an error dictionary."""
+    return {'error': msg}
+
+
+def search(string):
+    """Perform a search and return a dictionary of results. If an error occurs,
+    a dictionary with a single key "error" will be returned."""
+    url = base_url + string
+    res = http.get(url)
+    if not res.ok:
+        return error('Error: %r.' % res)
+    s = BeautifulSoup(res.content, 'html.parser')
+    data = s.body.find('script').text.strip().split(' = ')[1].strip(';')
+    data = loads(data)
+    products_data = data['products']['productsBySku']
+    products = {}
+    for product in products_data.values():
+        price = product['price']
+        current = price['current']
+        if 'unit' not in price:
+            price['unit'] = dict(per='each', price=current)
+        price = price['unit']
+        per = price['per']
+        price = price['price']
+        if per == 'per 100ml':
+            per = 'per litre'
+            price *= 10
+        elif per == 'per 100g':
+            per = 'per kg'
+            price *= 10
+        elif per == 'per 1g':
+            per = 'per kg'
+            price *= 1000
+        elif per == 'per 10g':
+            per = 'per kg'
+            price *= 100
+        if per not in products:
+            products[per] = []
+        sku = product['sku']
+        products[per].append(
+            dict(
+                sku=sku, name=product['name'],
+                price=current, per=price, url=base_url + sku,
+                weight=product['catchWeight']
+            )
+        )
+    if not products:
+        return error('No products to show.')
+    for per, data in products.copy().items():
+        products[per] = sorted(data, key=lambda thing: thing['per'])
+    return products
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ocado/', methods=['GET', 'POST'])
+def ocado_search():
+    string = request.form['string']
+    string = quote(string)
+    results = search(string)
+    return jsonify(results)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6223)
--- static/main.js
+++ static/main.js
@@ -1,32 +1,59 @@
-function loadSearchResults() {
+function loadSearchResults(e) {
+    let response = e.target.response
+    let data = JSON.parse(response)
+    clearElement(output)
+    if (data.error !== undefined) {
+        output.innerText = `Error: ${data.error}`
+    } else {
+        for (let per in data) {
+            let h3 = document.createElement("h3")
+            h3.innerText = per
+            output.appendChild(h3)
+            for (let product of data[per]) {
+                let h4 = document.createElement("h4")
+                let a = document.createElement("a")
+                a.innerText = `${product.name} ${product.weight}`
+                a.target = "_new"
+                a.href = product.url
+                h4.appendChild(a)
+                output.appendChild(h4)
+                let p = document.createElement("p")
+                p.innerHTML = `${pound}${product.price.toFixed(2)} (${pound}${product.per.toFixed(2)} ${per})`
+                output.appendChild(p)
+            }
+        }
+    }
     searchButton.disabled = false
     searchButton.value = "Search"
-    let html = frame.contentDocument || frame.contentWindow
-    if (html.document) html = html.document
-    output.innerText = "Works."
 }
 
-const frame = document.getElementById("frame")
-const baseUrl = "https://www.ocado.com/search?entry="
+function clearElement(e) {
+    // Below code based on the first answer at:
+    // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    while (e.firstChild) {
+        e.removeChild(e.firstChild)
+    }
+}
+
+const pound = "&pound;"
 const string = document.getElementById("string")
 const searchForm = document.getElementById("searchForm")
 const searchButton = document.getElementById("searchButton")
 const output = document.getElementById("output")
 
-window.onload = () => {
-    frame.hidden = true
-    frame.onload = loadSearchResults
-}
-
 searchForm.onsubmit = (e) => {
     e.preventDefault()
-    let value = escape(string.value)
+    let value = string.value
     if (!value) {
         return alert("You must search for something.")
     }
-    let url = baseUrl + value
     searchButton.disabled = true
     searchButton.value = "Loading..."
     output.innerText = "Loading results, please wait."
-    frame.src = url
+    let xhr = new XMLHttpRequest()
+    xhr.onload = loadSearchResults
+    xhr.open("POST", "/ocado/")
+    let fd = new FormData()
+    fd.append("string", string.value)
+    xhr.send(fd)
 }
